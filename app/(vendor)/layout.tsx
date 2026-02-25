9b532f3ad359879e5b37316f12772f3f2548b10f
AjayAR <ajayperies10@gmail.com>
2026-02-25 19:54:18 +0530

    fix footer bar in mobile view

--- app/(vendor)/layout.tsx
+++ app/(vendor)/layout.tsx
@@ -176,26 +176,7 @@ export default async function VendorLayout({
         <MobileFollowersSheet
           initialFollowers={followerUsers.map((f) => f.follower)}
         />
-        <Link
-          href="/"
-          className="flex flex-col items-center gap-0.5 text-xs text-gray-600 hover:text-blue-600 py-1 px-3"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            />
-          </svg>
-          Home
-        </Link>
+
       </nav>
     </>
   );
